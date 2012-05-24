8c2792cdd6d04a855e34533e900179c10cf3fda2
Noah Coad <noah@coad.net>
2012-05-24 01:30:22 -0500

    added default styling

--- PrivateNotes.php
+++ PrivateNotes.php
@@ -8,10 +8,19 @@ Author: Noah Coad
 Author URI: http://noahcoad.com
 License: GPL2
 */
-add_shortcode('note', 'sc_note');
 
-function sc_note($atts, $content = null) {
-	if (current_user_can('publish_posts'))
+
+add_action( 'wp_enqueue_scripts', 'prefix_add_my_stylesheet' );
+
+function prefix_add_my_stylesheet() {
+  wp_register_style( 'prefix-style', plugins_url('style.css', __FILE__) );
+  wp_enqueue_style( 'prefix-style' );
+}
+
+add_shortcode( 'note', 'sc_note' );
+
+function sc_note( $atts, $content = null ) {
+	if ( current_user_can( 'publish_posts' ))
 		return '<div class="note">'.$content.'</div>';
 	return '';
 }
